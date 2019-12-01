b0a64f99a2c850b56003b4df7ad8dd98d98b7680
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-12-01 18:27:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result_hash = {} 
+  data.each do |key, value| 
+    value.each do |new_value, names|
+      names.each do |name| 
+        
+        if !result_hash[name]
+          result_hash[name] = {} 
+        end
+        
+        if !result_hash[name][key]
+          result_hash[name][key] = [] 
+        end
+        
+        result_hash[name][key] << new_value.to_s 
+        
+      end
+    end
+  end
+  result_hash
 end
